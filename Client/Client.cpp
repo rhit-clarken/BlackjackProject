f74c47ae008d8ac11224b5d4948d7f2656e14732
kernanana <leek4@rose-hulman.edu>
2024-05-06 13:41:53 -0400

    Refactoring; initial handshake + ask clients if they want to play the game

---
+++ Client/Client.cpp
@@ -0,0 +1,88 @@
+#include <PNet/IncludeMe.h>
+#include <iostream>
+
+using namespace PNet;
+
+using namespace std;
+
+const char *SERVER_IP = "137.112.205.241";
+
+class ClientGame {
+public:
+	Socket socket;
+	void startClientEngine() {
+		std::cout << "Starting Client Engine" << std::endl;
+		if (Network::Initialize()) {
+			std::cout << "socket initialized" << std::endl;
+			if (socket.Create() == PResult::P_Sucess) {
+				std::cout << "client socket created" << std::endl;
+				connectToServer();
+			}
+			else {
+				std::cout << "client socket creation failed" << std::endl;
+			}
+		}
+		return;
+	}
+	void connectToServer() {
+		if (socket.Connect(IPEndpoint(SERVER_IP, 4790)) == PResult::P_Sucess) {
+			std::cout << "client connected to server" << std::endl;
+
+			Sleep(1500);
+
+			int result = 0;
+			int bytesSent = 0;
+			char handShake[10];
+			strcpy_s(handShake, "Hello\0");
+
+			std::cout << "sending handshake" << std::endl;
+			result = socket.Send(handShake, 10, bytesSent);
+			std::cout << result << std::endl;
+			std::cout << "doen sending handshake" << std::endl;
+			if (result != PResult::P_Sucess) {
+				std::cout << "Error sending initial handshake to the server" << std::endl;
+			}
+		}
+		else {
+			std::cout << "client socket connection/handshake failed" << std::endl;
+		}
+	}
+
+	void joinGame() {
+		
+		std::cout << "Attempting to join game" << std::endl;
+		char buffer[256];
+		int bytesReceived = 0;
+		int result = socket.Recv(buffer, 256, bytesReceived);
+		std::cout << buffer << std::endl;
+		if (result != PResult::P_Sucess) {
+			std::cout << "Error getting data from server" << std::endl;
+		}
+		else {
+			int bytesSent;
+			char userInput[10];
+			
+			std::cout << "Press 0 to join game; enter 1 to stand" << std::endl;
+			std::cin >> userInput;
+			result = socket.Send(userInput, 256, bytesSent);
+			if (result != PResult::P_Sucess) {
+				std::cout << "Error sending data to the server" << std::endl;
+			}
+		}
+
+		socket.Close();
+	}
+
+	ClientGame() {
+	}
+};
+
+
+int main() {
+	ClientGame newGame;
+	newGame.startClientEngine();
+	newGame.connectToServer();
+	newGame.joinGame();
+	system("pause");
+	Network::Shutdown();
+}
--- Server/ServerGame.cpp
+++ Server/ServerGame.cpp
@@ -8,57 +8,89 @@ using namespace std;
 
 class ServerGame {
 public:
-	int portNum;
+	Socket socket;
+	Socket clientSocket;
 	void startGameEngine() {
-		cout << "Starting Game Engine";
+		std::cout << "Starting Game Engine" << std::endl;
+		if (Network::Initialize()) {
+			std::cout << "socket initialized" << std::endl;
 
+			if (socket.Create() == PResult::P_Sucess) {
+				std::cout << "server socket created" << std::endl;
+				if (socket.Listen(IPEndpoint("0.0.0.0", 4790)) == PResult::P_Sucess) {
+					std::cout << "Socket listening on port 4790" << std::endl;
+					
+					acceptClient();
+				}
+				else {
+					std::cerr << "listening on port 4790 failed" << std::endl;
+				}
+				socket.Close();
+			}
+			else {
+				std::cerr << "server socket creation failed" << std::endl;
+			}
+		}
+		
+		return;
 	}
-	ServerGame(int port) {
-		portNum = port;
+
+	void acceptClient() {
+		if (socket.Accept(clientSocket) == PResult::P_Sucess) {
+			std::cout << "Accepted Connection" << std::endl;
+
+			//expect initial handshake
+			initialHandshake();
+
+			char buffer[256];
+			strcpy_s(buffer, "Would you like to join a blackjack table?\0");
+			int bytesSent = 0;
+
+			int result = clientSocket.Send(buffer, 256, bytesSent);
+
+			if (result != PResult::P_Sucess) {
+				std::cout << "Failed to ask client" << std::endl;
+			}
+			else {
+				char clientResponse[10];
+				int bytesReceived = 0;
+				int result = clientSocket.Recv(clientResponse,256,bytesReceived);
+				if (result != PResult::P_Sucess) {
+					std::cout << "Failed to recv client response" << std::endl;
+				}
+				else {
+					std::cout << clientResponse << std::endl;
+				}
+			}
+
+			socket.Close();
+		}
+		else {
+			std::cerr << "could not accept new connection" << std::endl;
+		}
+	}
+
+	void initialHandshake() {
+		char clientResponse[10];
+		int bytesReceived = 0;
+		int result = clientSocket.Recv(clientResponse, 10, bytesReceived);
+		if (result != PResult::P_Sucess) {
+			std::cout << "Failed to recv client handshake" << std::endl;
+		}
+		else {
+			std::cout << clientResponse << std::endl;
+		}
+	}
+
+	ServerGame() {
 	}
 };
 
 int main() {
-	if (Network::Initialize()) {
-		std::cout << "socket initialized" << std::endl;
-
-		Socket socket;
-		if (socket.Create() == PResult::P_Sucess) {
-			std::cout << "server socket created" << std::endl;
-			if (socket.Listen(IPEndpoint("0.0.0.0", 4790)) == PResult::P_Sucess) {
-				std::cout << "Socket listening on port 4790" << std::endl;
-				Socket newConnection;
-				if (socket.Accept(newConnection) == PResult::P_Sucess) {
-					std::cout << "Accepted Connection" << std::endl;
-
-					char buffer[256];
-					int bytesReceived = 0;
-					int result = PResult::P_Sucess;
-					while (result == PResult::P_Sucess) {
-						result = newConnection.Recv(buffer, 256, bytesReceived);
-						if (result != PResult::P_Sucess) {
-							break;
-						}
-						std::cout << buffer << std::endl;
-					}
-
-					socket.Close();
-				}
-				else {
-					std::cerr << "could not accept new connection" << std::endl;
-				}
-			}
-			else {
-				std::cerr << "listening on port 4790 failed" << std::endl;
-			}
-			socket.Close();
-		}
-		else {
-			std::cerr << "server socket creation failed" << std::endl;
-		}
-	}
-	Network::Shutdown();
+	ServerGame server;
+	server.startGameEngine();
 	system("pause");
-	return 0;
-
+	server.acceptClient();
+	system("pause");
+	Network::Shutdown();
 }
